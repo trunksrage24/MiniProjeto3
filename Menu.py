726c24a6b8c430ae5e0e93418d0da085500647ae
JoaoCarvalho2002 <jonitkick@gmail.com>
2022-12-27 16:27:23 +0000

    Comentários

--- Menu.py
+++ Menu.py
@@ -1,45 +1,44 @@
+#Imports de ficheiro "Imports" que guarda classes, cores, etc
 from Imports import *
 SCREEN=pygame.display.set_mode((1000,380))
 SCREEN.fill((0,0,0))
 
 def menuini():
-
+    SCREEN.fill((0,0,0))
     while True:  
         for event in pygame.event.get():
-                            #posição do constante mouse    
+            #posição do constante mouse    
             if event.type==pygame.MOUSEMOTION:
                 #posição do mouse    
                 pos=pygame.mouse.get_pos()    
-                                    
-
-                                    #start button
+                        
+                #start button
                 if start_but.collidepoint(pos):
                     #butão start e o "blit" "acende" o butão quando o rato passa por cima
                     pygame.draw.rect(SCREEN,WHITE,(290,200,190,70))
                     SCREEN.blit(textstart,(330,210))
                     pygame.display.update()
-                    #se o butão for carregado
+                    #se o butão for carregado(tem de ser carregado algumas vezes)#####
                     if pygame.mouse.get_pressed()[0] and start_but.collidepoint(pos):
                         print("Move on")
                         #provavelmente não a melhor maneira de passar pra outro screen mas foi oque deu
                         from Gamefile import SCREENGAME
-                
-                                                      
-                                            
+                                  
                 #exit button
                 if exit_but.collidepoint(pos):
                     #butão start e o "blit" "acende" o butão quando o rato passa por cima
                     SCREEN.blit(textquit,(550,200))
                     pygame.draw.rect(SCREEN,WHITE,(550,200,190,70))
                     pygame.display.update()
-                    #se o butão for carregado
+                    #se o butão for carregado(tem de ser carregado algumas vezes)#####
                     if pygame.mouse.get_pressed()[0] and exit_but.collidepoint(pos):
                         SCREEN.blit(textquit,(590,210))
                         #sai do jogo
                         print("quit")
                         pygame.quit()   
                         sys.exit()
-        SCREEN.fill((0,0,0))            
+                        
+                    
         SCREEN.blit(textHO,(310,80))
 
         #startbutton default sem interferencia do MOUSE
