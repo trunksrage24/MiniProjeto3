e15f3f0a2310c22ea8d3337938e96465fd39da03
Vitor Daniel <vitor.daniel@outlook.pt>
2022-12-27 16:02:05 +0000

    started a basic structure for player; drawn images for player and boats

--- player.py
+++ player.py
@@ -5,4 +5,21 @@ import math
 import numpy as np
 import time
 
+#colors
+White = (255, 255, 255)
+Black = (0, 0, 0)
+Red = (235, 25, 25)
+Green = (25, 235, 25)
+Blue = (25, 25, 235)
+Yellow = (255, 255, 0)
+Steel = (176, 196, 222)
 
+# game surface
+Window = py.display.set_mode(1000, 380)
+
+# variables
+player_pos = (0, 0) #temporary
+
+# player image
+player_im = py.image.load("sprites/Cannon.png")
+Player = py.draw.circle(Window, False, player_pos, player_im)
